c996f4e29fc12e6d0816f378796550c54c33dcec
Andrew Waller <andrewhwaller@gmail.com>
2019-04-10 01:51:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const currentUser = 'Andrew Waller';
 let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser}.charAt(0)`
